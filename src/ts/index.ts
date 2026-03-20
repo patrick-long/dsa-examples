498c1490c048e28930d6e88b4a0537f3692a318b
Patrick Long <patrick@deepspacerobots.com>
2026-03-20 13:00:14 -0500

    implement TS linked list node

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+import LinkedListNode from '../data-structures/linked-list/linked-list-node.ts';
+
+const first = new LinkedListNode(1);
+const second = new LinkedListNode(2, null, first);
+const third = new LinkedListNode(3, null, second);
+
+console.log({ first, second, third });
+
